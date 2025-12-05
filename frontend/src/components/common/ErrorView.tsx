01ad3d36c95b2114340027d6cb5d6e66fb6c913b
Reo05-code <t.asaishi05@gmail.com>
2025-12-05 15:18:06 +0900

    style(#21): ErrorView と LockIcon をフォーマット

--- frontend/src/components/common/ErrorView.tsx
+++ frontend/src/components/common/ErrorView.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function ErrorView({ message }: Props) {
 	if (!message) return null
-	
+
 	return (
 		<div className="text-sm text-red-600 mb-2" role="alert">
 			{message}
--- frontend/src/components/ui/LockIcon.tsx
+++ frontend/src/components/ui/LockIcon.tsx
@@ -1,16 +1,16 @@
 export default function LockIcon() {
 	return (
-		<svg 
-			aria-hidden="true" 
-			className="h-10 w-10 text-white" 
-			fill="currentColor" 
-			viewBox="0 0 24 24" 
+		<svg
+			aria-hidden="true"
+			className="h-10 w-10 text-white"
+			fill="currentColor"
+			viewBox="0 0 24 24"
 			xmlns="http://www.w3.org/2000/svg"
 		>
-			<path 
-				clipRule="evenodd" 
-				d="M12 1.5a5.25 5.25 0 00-5.25 5.25v3a3 3 0 00-3 3v6.75a3 3 0 003 3h10.5a3 3 0 003-3v-6.75a3 3 0 00-3-3v-3A5.25 5.25 0 0012 1.5zm-3.75 5.25a3.75 3.75 0 107.5 0v3h-7.5v-3z" 
-				fillRule="evenodd" 
+			<path
+				clipRule="evenodd"
+				d="M12 1.5a5.25 5.25 0 00-5.25 5.25v3a3 3 0 00-3 3v6.75a3 3 0 003 3h10.5a3 3 0 003-3v-6.75a3 3 0 00-3-3v-3A5.25 5.25 0 0012 1.5zm-3.75 5.25a3.75 3.75 0 107.5 0v3h-7.5v-3z"
+				fillRule="evenodd"
 			/>
 		</svg>
 	)
