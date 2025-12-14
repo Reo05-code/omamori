b06ebcd23c660a11408c3fed17f797cccf4a11dc
Reo05-code <t.asaishi05@gmail.com>
2025-12-14 16:16:51 +0900

    (#65) refactor(membership): rubocopの自動修正

--- backend/app/models/membership.rb
+++ backend/app/models/membership.rb
@@ -2,7 +2,7 @@ class Membership < ApplicationRecord
   belongs_to :organization
   belongs_to :user
 
-  enum role: { worker: 0, admin: 1 }
+  enum :role, { worker: 0, admin: 1 }
 
   validates :user_id, uniqueness: { scope: :organization_id }
 end
