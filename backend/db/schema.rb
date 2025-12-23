46191feea833f6bd99d2f71a9dbc0bc74c2bce90
Reo05-code <t.asaishi05@gmail.com>
2025-12-24 00:21:00 +0900

    (#89) feat(DB): users に home_location と home_radius を追加するマイグレーション  - マイグレーションファイルを追加/更新  - schema.rb を更新  - database.yml を postgis adapter に切替

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_21_080452) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_23_133448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "invitations", force: :cascade do |t|
     t.bigint "inviter_id", null: false
@@ -46,26 +47,17 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_21_080452) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", default: false
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.string "phone_number"
-    t.string "avatar_url"
-    t.string "email"
-    t.json "tokens"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.jsonb "settings", default: {}, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  create_table "spatial_ref_sys", primary_key: "srid", id: :integer, default: nil, force: :cascade do |t|
+    t.string "auth_name", limit: 256
+    t.integer "auth_srid"
+    t.string "srtext", limit: 2048
+    t.string "proj4text", limit: 2048
+    t.check_constraint "srid > 0 AND srid <= 998999", name: "spatial_ref_sys_srid_check"
   end
+
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'geography' for column 'home_location'
+
 
   create_table "work_sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
