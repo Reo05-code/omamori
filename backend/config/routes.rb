c639b3e582d7071b081ef338dfd462f3c9caf11d
Reo05-code <t.asaishi05@gmail.com>
2025-12-26 16:32:44 +0900

    (#90) feat: SafetyLogsコントローラ用のルーティングを追加
    - work_sessions/:work_session_id/safety_logs にネストされたルートを追加
    - index と create アクションを定義

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -36,6 +36,9 @@ Rails.application.routes.draw do
           post :finish
           post :cancel
         end
+
+        # SafetyLog（生存報告ログ）
+        resources :safety_logs, only: %i[index create]
       end
     end
   end
