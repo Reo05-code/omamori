83d1b436c2735284b130a65549deb574f7327ef9
Reo05-code <t.asaishi05@gmail.com>
2026-01-21 01:57:27 +0900

    (#189) chore: Planner の seed テストは「単一テストのみ」を想定しているためdescribe削除

--- frontend/e2e/seed.spec.ts
+++ frontend/e2e/seed.spec.ts
@@ -1,8 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Seed', () => {
-  test('seed', async ({ page }) => {
-    // Generate code here.
-    await expect(page).toBeDefined();
-  });
+test('seed', async ({ page }) => {
+  // Generate code here.
+  await expect(page).toBeDefined();
 });
